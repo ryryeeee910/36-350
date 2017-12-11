cd8f0c29263525d896e53f6cc50c3bef530bef52
Ryan Chan <ryryeeee@Ryans-MacBook-Pro.local>
2017-12-10 19:45:12 -0500

    Question 20 of Final Project

---
+++ hello_aliens.R
@@ -0,0 +1,9 @@
+Hello, Aliens. Welcome to planet Earth. Our planet is made up 7 continents, each
+with its own set of unique traits. I am from North America, a landmass with
+many different climates and cultures throughout. A continent like Africa,
+however, has a much warmer climate and different wildlife. Each continent has
+its own personality, so I encourage you to visit each one during your stay.
+The best way to get to know planet Earth is to explore for yourselves! I have
+a few questions for you, however. What planet do you all come from? How is it
+different from what you have seen so far of planet Earth? Finally, could you 
+take me there? I'd love to see what makes you all special.
